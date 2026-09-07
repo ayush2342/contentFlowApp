05117de40d9d861d67a68de1ff0032c39091d1d8
Ayush Sharma <ayushsharma2342@gmail.com>
2026-09-07 20:55:56 +0530

    Markdown Changes

--- frontend/digital-output/src/utils/inlineHtml.jsx
+++ frontend/digital-output/src/utils/inlineHtml.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ALLOWED_TAGS = new Set(['I', 'EM', 'B', 'STRONG', 'SPAN', 'SUP', 'A', 'BR']);
+const ALLOWED_TAGS = new Set(['I', 'EM', 'B', 'STRONG', 'SPAN', 'SUP', 'A', 'BR', 'MARK']);
 
 const getAttr = (el, name) => {
   try {
@@ -44,6 +44,15 @@ const walkNodes = (nodes, keyPrefix = 'n', options = {}) => {
 
     if (tag === 'BR') {
       out.push(<br key={key} />);
+      return;
+    }
+
+    if (tag === 'MARK') {
+      out.push(
+        <mark key={key} className="cendoc-mark">
+          {children}
+        </mark>
+      );
       return;
     }
 
